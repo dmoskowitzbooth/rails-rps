9ec4ecbf4a9bdf3ac5ffd411978a2d6f563e7445
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-04-23 21:28:25 +0000

    fin

--- app/controllers/rock_controller.rb
+++ app/controllers/rock_controller.rb
@@ -1,7 +1,21 @@
 class RockController < ApplicationController
   def prock
     @random_move=["rock", "paper", "scissors"].sample
-    render({ :template=>"game_templates/play_rock"})
-    
+    render({ :template=>"game_templates/play_rock"}) 
+  end
+
+  def ppaper
+    @random_move=["rock", "paper", "scissors"].sample
+    render({ :template=>"game_templates/play_paper"}) 
+  end
+
+  def pscissors
+    @random_move=["rock", "paper", "scissors"].sample
+    render({ :template=>"game_templates/play_scissor"}) 
+  end
+
+  def home
+    @random_move=["rock", "paper", "scissors"].sample
+    render({ :template=>"game_templates/home"}) 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
 get("/rock", { :controller =>"rock", :action =>"prock"})
+get("/paper", { :controller =>"rock", :action =>"ppaper"})
+get("/scissors", { :controller =>"rock", :action =>"pscissors"})
+get("/", { :controller =>"rock", :action =>"home"})
 end
